e765f315b0337098a07d3575919c5941d3865351
Richard Lewis <richard.lewis@gold.ac.uk>
2016-01-13 23:39:54 +0000

    Working out more types

--- src/MuSAK/Annotations/Types.hs
+++ src/MuSAK/Annotations/Types.hs
@@ -1,14 +1,24 @@
-module MuSAK.Annotations.Types (Mark(..)) where
+module MuSAK.Annotations.Types ( Mark(..)
+                               , Page(..)
+                               , Point
+                               , Shape(..)
+                               , Weight
+                               , connectionThreshold
+                               , end
+                               , start
+                               , weight ) where
 
-import Control.Applicative
-import Control.Monad
-import Data.Csv
-import Data.Time.Format (parseTime, formatTime)
-import System.Locale (TimeLocale, defaultTimeLocale, timeFmt)
-import Data.Time.LocalTime
-import Data.Vector as DV
-import Data.ByteString (ByteString)
+import           Control.Applicative
+import           Control.Monad
+import           Data.ByteString (ByteString)
+import           Data.Csv
+import           Data.Time.Format (TimeLocale, defaultTimeLocale, timeFmt, parseTime, formatTime)
+import           Data.Time.LocalTime
+import           Data.Vector as DV
 import qualified Data.ByteString.Char8 as BC
+
+type Point = (Int, Int)
+type Weight = Double
 
 data Mark = Mark { color  :: !ByteString
                  , pen    :: !Int
@@ -39,6 +49,23 @@ instance ToRecord Mark where
                                              , toField eY
                                              , toField t ]
 
+start :: Mark -> Point
+start (Mark { startX = x, startY = y }) = (x, y)
+
+end :: Mark -> Point
+end (Mark { endX = x, endY = y }) = (x, y)
+
+connectionThreshold :: Weight
+connectionThreshold = 1.0
+
+-- FIXME Should we retain the (end a) == (start b) check? It might
+-- turn out that this check fails in the real data.
+weight :: Mark -> Mark -> Weight
+-- weight a b | end a == start b = asSeconds (time b) - asSeconds (time a)
+--            | otherwise        = connectionThreshold
+weight a b = asTime b - asTime a
+  where asTime = asSeconds . time
+
 locale :: TimeLocale
 locale = defaultTimeLocale { timeFmt = "%H:%M:%S%Q" }
 
@@ -55,3 +82,21 @@ instance FromField TimeOfDay where
 
 instance ToField TimeOfDay where
   toField s = BC.pack (formatTime locale format s)
+
+asSeconds :: TimeOfDay -> Double
+asSeconds (TimeOfDay { todHour = h, todMin = m, todSec = s }) = hs + ms + (realToFrac s)
+  where
+    hs = (realToFrac h) / 60.0 / 60.0
+    ms = (realToFrac m) / 60.0
+
+data Page = Page { pg_marks :: DV.Vector Mark
+                 , pg_sourceFile :: FilePath } deriving (Show)
+
+instance Eq Page where
+  (==) (Page { pg_sourceFile = a }) (Page { pg_sourceFile = b }) = a == b
+
+data Shape = Shape { sh_marks :: DV.Vector Mark
+                   , sh_label :: String } deriving (Show)
+
+instance Eq Shape where
+  (==) (Shape { sh_marks = a }) (Shape { sh_marks = b }) = undefined

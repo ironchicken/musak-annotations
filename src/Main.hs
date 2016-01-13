ae069280e0146faa845fd7373fac0d1d65217b1d
Richard Lewis <richard.lewis@gold.ac.uk>
2016-01-13 23:47:10 +0000

    Implemented pageLoad function

---
+++ src/Main.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import qualified Data.ByteString.Lazy as BL
+import           Data.Csv
+import qualified Data.Vector as V
+import           MuSAK.Annotations.Types
+import           System.Environment (getArgs)
+
+loadPage :: FilePath -> IO Page
+loadPage f = do
+  csvData <- BL.readFile f
+  case decode NoHeader csvData of
+    Left err    -> error $ "Could not parse " ++ f ++ ": " ++ err
+    Right marks -> return $ Page { pg_marks = marks
+                                 , pg_sourceFile = f }
+
+main :: IO ()
+main = do
+  (a:_) <- getArgs
+  p <- loadPage a
+  putStrLn $ show p
+

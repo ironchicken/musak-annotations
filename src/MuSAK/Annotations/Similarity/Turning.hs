59b0adfd135279597c6aacf7f0c3f7ffc1e8e6b5
Richard Lewis <richard.lewis@gold.ac.uk>
2016-02-01 22:36:10 +0000

    Removed tangent from turning function

--- src/MuSAK/Annotations/Similarity/Turning.hs
+++ src/MuSAK/Annotations/Similarity/Turning.hs
@@ -20,17 +20,8 @@ mapWithPrev _ _    []     = []
 turningRep :: Shape -> TurningShape
 turningRep s = TurningShape s legs (perimeter s)
   where
-    legs = mapWithPrev leg (Leg 0 0) $ zip (tangent (head (sh_marks s)) : (sh_marks s)) ((sh_marks s) ++ [tangent (head (sh_marks s))])
+    legs = mapWithPrev leg (Leg 0 0) $ zip (sh_marks s) ((tail $ sh_marks s) ++ [head $ sh_marks s])
     leg (m1,m2) (Leg prev _) = Leg (turningAngle s prev m1 m2) (markLenScaled s m2)
-    tangent m = m { startX = (fst (start m)) - 5
-                  , startY = midPoint (startY m) (endY m)
-                  , endX   = (fst (end m)) + 5
-                  , endY   = midPoint (startY m) (endY m) }
-      where
-        midPoint :: Int -> Int -> Int
-        midPoint y1 y2 | y1 < y2 = y1 + ((y2 - y1) `div` 2)
-                       | y1 > y2 = y2 + ((y1 - y2) `div` 2)
-                       | otherwise = y1
 
 turningAngle :: Shape -> Angle -> Mark -> Mark -> Angle
 turningAngle s base m1 m2 = a

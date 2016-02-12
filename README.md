b06eef878721d552c0e42e099e983223efd1fdc6
Richard Lewis <richard.lewis@gold.ac.uk>
2016-02-12 00:00:51 +0000

    Added license and copyright notices

--- README.md
+++ README.md
@@ -97,3 +97,23 @@ there's no corresponding "uninstall".
 
 The next phase of this software will be to implement some shape
 distance measures and then cluster the shapes by similarity.
+
+## License
+
+Copyright (C) 2015, 2016 Richard Lewis
+
+Author: richard.lewis@gold.ac.uk
+
+musak-annotations is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+musak-annotations is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with musak-annotations. If not, see
+<http://www.gnu.org/licenses/>.
--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,24 @@
+-- musak-annotations - A tool for analysing the annotations produced
+-- by the MuSAK system
+--
+-- Copyright (C) 2015, 2016 Richard Lewis
+-- Author: richard.lewis@gold.ac.uk
+
+-- This file is part of musak-annotations.
+
+-- musak-annotations is free software: you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation, either version 3 of
+-- the License, or (at your option) any later version.
+
+-- musak-annotations is distributed in the hope that it will be
+-- useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+-- of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with musak-annotations. If not, see <http://www.gnu.org/licenses/>.
+
 module Main where
 
 import           Control.Monad (unless)
